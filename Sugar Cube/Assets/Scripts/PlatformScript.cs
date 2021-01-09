fdef400a65840edde4e5704b40f4052d9c668a9a
EnisTrabelsi <74650952+EnisTrabelsi@users.noreply.github.com>
2021-01-09 16:29:27 +0100

    Platform Spawer
    Added the platform spawner that consists of static platforms and speed platforms/speed platforms.

---
+++ Sugar Cube/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    public float move_Speed = 2f;
+    public float bound_Y = 6f;
+
+    public bool moving_Platform_Left, moving_Platform_Right, is_Platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() {
+        Vector2 temp = transform.position;
+        temp.y += move_Speed * Time.deltaTime;
+        transform.position = temp;
+
+        if(temp.y >= bound_Y) {
+            gameObject.SetActive(false);
+        }
+    }
+}
