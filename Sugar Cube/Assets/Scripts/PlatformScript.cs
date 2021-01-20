144afb34b7f3d97e68a507dc8fd12be3bb971f39
Nicole  Mifsud <nicole.mifsud.a101123@mcast.edu.mt>
2021-01-20 17:31:56 +0100

    Update
    Game Manager
    Sound Manager
    Player Bounds

--- Sugar Cube/Assets/Scripts/PlatformScript.cs
+++ Sugar Cube/Assets/Scripts/PlatformScript.cs
@@ -8,7 +8,7 @@ public class PlatformScript : MonoBehaviour
     public float move_Speed = 2f;
     public float bound_Y = 6f;
 
-    public bool moving_Platform_Left, moving_Platform_Right, is_Platform;
+    public bool moving_Platform_Left, moving_Platform_Right,is_Spike, is_Platform;
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +30,48 @@ public class PlatformScript : MonoBehaviour
         if(temp.y >= bound_Y) {
             gameObject.SetActive(false);
         }
+    }//move
+    
+    void OnTriggerEnter2D(Collider2D target) 
+    {
+        if(target.tag =="Player")
+        {
+            if(is_Spike)
+            {
+                target.transform.position = new Vector2 (1000f,1000f);
+                SoundManager.instance.GameOverSound();
+                GameManager.instance.RestartGame();
+            }
+        }
+    } //on trigger enter
+
+    void OnCollisionEnter2D(Collision2D target) 
+    {
+        if(target.gameObject.tag == "Player")
+        {
+            if(is_Platform)
+            {
+                SoundManager.instance.LandSound();
+            }
+        }
+
+    }// on Collison Enter
+
+    void OnCollisionStay2D(Collision2D target) 
+    {
+        if(target.gameObject.tag == "Player")
+        {
+            if(moving_Platform_Left)
+            {
+                target.gameObject.GetComponent<SugarCube>().PlatformMove(-1f);
+            }
+
+            if(moving_Platform_Right)
+            {
+                target.gameObject.GetComponent<SugarCube>().PlatformMove(1f);
+            }
+        }
     }
-}
+
+}//class
+
