ad54be3180b331753816d9028fb969e41b0ad58f
Nicole  Mifsud <nicole.mifsud.a101123@mcast.edu.mt>
2021-02-02 15:29:09 +0100

    Score update
    when player lands on moving platform the score increases

--- Sugar Cube/Assets/Scripts/Power Ups/PUSpawner.cs
+++ Sugar Cube/Assets/Scripts/Power Ups/PUSpawner.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class PUSpawner : MonoBehaviour
 {
-    // [SerializeField]
-    // private GameObject[] _powerups;
+    [SerializeField]
+    private GameObject[] _powerups;
 
-    // private bool _stopSpawning = false;
+    private bool _stopSpawning = false;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     StartCoroutine(SpawnPowerupRoutine());
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPowerupRoutine());
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
+    // Update is called once per frame
+    void Update()
+    {
 
-    // }
+    }
     
-    // IEnumerator SpawnPowerupRoutine()
-    // {
-    //     while(_stopSpawning == false)
-    //     {
-    //         Vector3 posToSpawn = new Vector3(Random.Range(-2.4f,2.4f),4f,0);
-    //         int randomPowerup = Random.Range(0,_powerups.Length);
-    //         Instantiate(_powerups[randomPowerup],posToSpawn,Quaternion.identity);
-    //         yield return new WaitForSeconds(Random.Range(6,10));
-    //     }
-    // }
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while(_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-2.4f,2.4f),4f,0);
+            int randomPowerup = Random.Range(0,_powerups.Length);
+            Instantiate(_powerups[randomPowerup],posToSpawn,Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(6,10));
+        }
+    }
 
-    // public void OnPlayerDeath()
-    // {
-    //     _stopSpawning = true;
-    // }
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
 }//class
--- Sugar Cube/Assets/Scripts/Power Ups/PowerUp.cs
+++ Sugar Cube/Assets/Scripts/Power Ups/PowerUp.cs
@@ -4,49 +4,49 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // [SerializeField]
-    // private float _speed = 3f;
+    [SerializeField]
+    private float _speed = 3f;
 
-    // [SerializeField]
-    // private int _powerupID;
+    [SerializeField]
+    private int _powerupID;
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     transform.Translate(Vector3.down * Time.deltaTime * _speed);
-    //     //if the powerup position on the y axis is smaller than -6f destroy the powerup
-    //     if(transform.position.y < -6f)
-    //     {
-    //         Destroy(this.gameObject);
-    //     }
-    // }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+        //if the powerup position on the y axis is smaller than -6f destroy the powerup
+        if(transform.position.y < -6f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
-    // void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if(other.tag == "Player")
-    //     {
-    //         SugarCube player = other.GetComponent<SugarCube>();
-    //         if(player != null)
-    //         {
-    //             switch(_powerupID)
-    //             {
-    //                 case 0:
-    //                     player.SpeedBoostActive();
-    //                     Debug.Log("_speed Boost Activated");
-    //                     break;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SugarCube player = other.GetComponent<SugarCube>();
+            if(player != null)
+            {
+                switch(_powerupID)
+                {
+                    case 0:
+                        player.SpeedBoostActive();
+                        Debug.Log("_speed Boost Activated");
+                        break;
 
-    //                 case 1:
-    //                     //player.SpeedBoostActive();
-    //                     Debug.Log("_speed Boost Activated");
-    //                     break;
+                    case 1:
+                        //player.SpeedBoostActive();
+                        Debug.Log("_speed Boost Activated");
+                        break;
 
-    //                 default:
-    //                     Debug.Log("Default Value");
-    //                     break;
+                    default:
+                        Debug.Log("Default Value");
+                        break;
 
-    //             }
-    //         }
-    //         Destroy(this.gameObject);
-    //     }
-    // }
+                }
+            }
+            Destroy(this.gameObject);
+        }
+    }
 }
