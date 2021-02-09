2171be91ac9d2106249b8dbf0c7f69d470ac8599
Nicole  Mifsud <nicole.mifsud.a101123@mcast.edu.mt>
2021-02-09 15:14:07 +0100

    restructuring files 15_13

---
+++ Assets/Scripts/BG/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake() 
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void RestartGame(){
+        Invoke("RestartAfterTime", 2f);
+    }
+
+    void RestartAfterTime()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}//class
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    //public bool EnemyMove, EnemyFlip;
+
+    public int enemyID;
+    private float currentSpeed = 2f;
+    private float x, y, z;
+
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        //currentSpeed = Random.Range (minSpeed, maxSpeed);
+        //x = -4f;
+        //y = Random.Range (-4.0f, 4.0f);
+        //z = 0.0f;
+        //transform.position = new Vector3 (x, y, z);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(enemyID == 1)
+        {
+            EnemyMove();
+        }
+        else if(enemyID == 2)
+        {
+            EnemyFlip();
+        }
+
+        //EnemyFlip();
+
+        //translate the enemy down
+        //it needs to move at a speed of 4m/s
+
+        
+
+        
+    }
+    void EnemyMove()
+    {
+        float amountToMove = currentSpeed * Time.deltaTime;
+        transform.Translate (Vector3.right  * amountToMove);
+        if (transform.position.x >= 4f) 
+        {
+            y = Random.Range (-4.0f, 4.0f);
+            transform.position = new Vector3 (-4, y, z);
+        }
+    }
+
+    void EnemyFlip()
+    {
+        float amountToMove = currentSpeed * Time.deltaTime;
+        transform.Translate (Vector3.left  * amountToMove);
+        if (transform.position.x <= -4f) 
+        {
+            y = Random.Range (-4.0f, 4.0f);
+            transform.position = new Vector3 (4, y, z);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Player")
+        {
+            Destroy(target.gameObject);
+            Destroy(this.gameObject);
+            SoundManager.instance.DeathSound();
+            GameManager.instance.RestartGame();
+        }
+        
+    }
+}//class
